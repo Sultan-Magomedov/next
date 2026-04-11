c11cc474623793ab28127c9d105799de915257c6
Magomedov Sultan <sultanatus008@mail.ru>
2026-04-11 23:46:39 +0300

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { TypographyH1 } from "@/components/ui/typographyH1";
+import { TypographyH3 } from "@/components/ui/typographyH3";
 
 
 export default function Home() {
   return (
     <div>
       <main >
+        <TypographyH1>Hello World</TypographyH1>
+        <TypographyH3>Hello World</TypographyH3>
         <Button variant="outline">Click me</Button>
       </main>
     </div>
